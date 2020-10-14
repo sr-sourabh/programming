70ce36031f014b3f6f1e13204faeb1ff8745cd1f
sr-sourabh <shourabh@glabbr.com>
2020-10-14 21:12:06 +0530

    final additions of test cases

--- cp/assessment2/24.cpp
+++ cp/assessment2/24.cpp
@@ -11,7 +11,11 @@ using namespace std;
 typedef long long ll;
 
 ll prime = 1000000009;
-ll base = 62;                   //considering a-z A-Z and 0-9 values
+ll base = 62;                               //considering a-z A-Z and 0-9 values
+int subStringStartPos = -1;                 //start position of max length sub string
+
+//Returns the length of maximum substring in a and b using binary search
+int binarySearchForMaxLen(string a, string b);
 
 //Given two string a, b and length len, check if a and b both contain a substring of length = len
 int checkIfStringWithLengthIsSubString(string a, string b, int len);
@@ -25,15 +29,25 @@ int checkIfStringMatchBruteForce(string a, unordered_set<int> startPositionsOfA,
 //helper functions
 void printMap(unordered_map<ll, unordered_set<int>> map);
 void unitTest();
+void displaySubString(string a, int len);
 
 int main(){
-    //Ans should be 26 as we have a to z in between both
-    string a = "qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqabcdefghijklmnopqrstuvwxyzpppppppppppppppppppppppppppppppppppp";
-    string b = "fffffffffabcdefghijklmnopqrstuvwxyzkkkkkkkkkkkkkkkkkkk";
-
     unitTest();
-
     return 0;
+}
+
+int binarySearchForMaxLen(string a, string b){
+    int left = 0;
+    int right = min(a.size(), b.size()) + 1;
+
+    while(left < right){
+        int mid = (right + left) / 2;
+        if(checkIfStringWithLengthIsSubString(a,b, mid))
+            left = mid + 1;
+        else
+            right = mid;
+    }
+    return (left - 1);
 }
 
 int checkIfStringWithLengthIsSubString(string a, string b, int len){
@@ -56,9 +70,9 @@ int checkIfStringWithLengthIsSubString(string a, string b, int len){
     }
 
     while(i < b.size()){
-        hashVal = ((hashVal * base) + b[i] - (b[i + 1 - len] * final)) % prime;
+        hashVal = ((hashVal * base) + b[i] - (b[i - len] * final)) % prime;
         if(hashVal < 0) hashVal = hashVal + prime;
-        cout<<b[i + 1 - len]<<" "<<b[i]<<endl;
+
         if(map.count(hashVal) > 0){
             result = checkIfStringMatchBruteForce(a, map[hashVal], b, (i + 1 - len), len);
             if(result) return result;
@@ -76,7 +90,10 @@ int checkIfStringMatchBruteForce(string a, unordered_set<int> startPositionsOfA,
         for(i = 0; i < len; i++){
             if(a[startPositionOfA + i] != b[startPosOfB + i]) break;
         }
-        if(i == len) return 1;
+        if(i == len){
+            subStringStartPos = startPositionOfA;
+            return 1;
+        }
     }
     return result;
 }
@@ -99,15 +116,56 @@ unordered_map<ll, unordered_set<int>> getRollingHashMap(string a, int len){
         hashVal = ( (hashVal * base) + a[i] - ( a[i - len] * final ) ) % prime;
         if(hashVal < 0) hashVal = hashVal + prime;
         if(map.count(hashVal) > 0){
-            map[hashVal].insert(i - len);
+            map[hashVal].insert(i - len + 1);
         }
         else{
-            //starting position of string is i - len
-            map[hashVal] = {i - len};
+            //starting position of string is i - len + 1
+            map[hashVal] = {i - len + 1};
         }
         i++;
     }
     return map;
+}
+
+void unitTest(){
+    string a = "qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqabcdefghijklmnopqrstuvwxyzpppppppppppppppppppppppppppppppppppp";
+    string b = "fffffffffabcdefghijklmnopqrstuvwxyzkkkkkkkkkkkkkkkkkkk";
+    cout<<"Test case 1\nString a: "<<a<<endl<<"String b: "<<b<<endl;
+    int len = binarySearchForMaxLen(a, b);
+    cout<<"Expected: 26\tOutput: "<<len<<endl;
+    displaySubString(a, len);
+
+    a = "abcdefghij";
+    b = "def";
+    cout<<"Test case 2\nString a: "<<a<<endl<<"String b: "<<b<<endl;
+    len = binarySearchForMaxLen(a, b);
+    cout<<"Expected: 3\tOutput: "<<len<<endl;
+    displaySubString(a, len);
+
+    a = "abcccc";
+    b = "ggggggggggggggggggggabcccclllllllllllllllllllllllllll11111112222222";
+    cout<<"Test case 3\nString a: "<<a<<endl<<"String b: "<<b<<endl;
+    len = binarySearchForMaxLen(a, b);
+    cout<<"Expected: 6\tOutput: "<<len<<endl;
+    displaySubString(a, len);
+
+    a = "111111112222222222229999999999shourabh0000000000000009999999999999999";
+    b = "3333333333333333333333rabh8888888888888888";
+    cout<<"Test case 4\nString a: "<<a<<endl<<"String b: "<<b<<endl;
+    len = binarySearchForMaxLen(a, b);
+    cout<<"Expected: 4\tOutput: "<<len<<endl;
+    displaySubString(a, len);
+
+    a = "IIIT Bangalore";
+    b = "IIIT Bangalore";
+    cout<<"Test case 5\nString a: "<<a<<endl<<"String b: "<<b<<endl;
+    len = binarySearchForMaxLen(a, b);
+    cout<<"Expected: 14\tOutput: "<<len<<endl;
+    displaySubString(a, len);
+}
+
+void displaySubString(string a, int len){
+    cout<<"One of the maximum substring: "<<a.substr(subStringStartPos, len)<<endl<<endl;
 }
 
 void printMap(unordered_map<ll, unordered_set<int>> map){
@@ -121,11 +179,4 @@ void printMap(unordered_map<ll, unordered_set<int>> map){
         c += e.second.size();
     }
     cout<<"Total: "<<c<<endl;
-}
-
-
-void unitTest(){
-    string a = "qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqabcdefghijklmnopqrstuvwxyzpppppppppppppppppppppppppppppppppppp";
-    string b = "fffffffffabcdefghijklmnopqrstuvwxyzkkkkkkkkkkkkkkkkkkk";
-    cout<<checkIfStringWithLengthIsSubString(a, b, 26)<<endl;
 }
